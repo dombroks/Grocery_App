2f920965a99b9761022393eb2fa0025243b77a7b
dombroks <tinoutech@gmail.com>
2020-07-28 20:43:54 +0100

    modification

---
+++ lib/Services/firebaseDatabase.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class firebaseDatabase {
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final DatabaseReference _reference = FirebaseDatabase.instance.reference();
+
+
+}
+
+
