9442344f62d83d5d4079655cd71202aa541664ba
dombroks <tinoutech@gmail.com>
2020-08-23 23:17:10 +0100

    profile screen

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -10,54 +10,67 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(10),
-          width: double.infinity,
-          height: size.height * 0.15,
-          color: Colors.amber,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 100,
-                height: 100,
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://th.bing.com/th/id/OIP.ju1vmgfjRSJnPU4fcOCyJwHaE8?pid=Api&w=1200&h=800&rs=1"),
+    return Container(
+      color: Colors.grey[300],
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            height: size.height * 0.15,
+            color: Colors.white,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://th.bing.com/th/id/OIP.ju1vmgfjRSJnPU4fcOCyJwHaE8?pid=Api&w=1200&h=800&rs=1"),
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Jessica alison",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 23,
+                SizedBox(
+                  width: 20,
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Jessica alison",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 23,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("tinoutech@gmail.com"),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("0777 000 000"),
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
-      ],
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "tinoutech@gmail.com",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "0777 000 000",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
