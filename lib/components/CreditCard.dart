afe9f0090deb1a90c1eb71be676559f4db700223
dombroks <tinoutech@gmail.com>
2020-10-22 17:53:07 +0100

    ++

--- lib/components/CreditCard.dart
+++ lib/components/CreditCard.dart
@@ -3,26 +3,66 @@ import 'package:flutter/material.dart';
 class CreditCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Dom Broks",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.delete_outline,
+                    color: Colors.grey,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
               Text(
-                "Dom Broks",
+                "· · · ·  · · · ·  · · · ·  5467",
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 15,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              )
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Month / Year",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "06 / 2022",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(children: [
+                Text(
+                  "Commercial Bank",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ]),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
