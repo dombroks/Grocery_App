bf96e1f867830b917a8f1a82f1a814bec78d45a8
dombroks <tinoutech@gmail.com>
2020-10-22 19:35:40 +0100

    ++

--- lib/components/AddCard.dart
+++ lib/components/AddCard.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/Screens/AddCardScreen.dart';
 
 class AddCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Card(
-        elevation: 4,
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-              height: 190,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.add,
-                      size: 31,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      "Add a card",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Card(
+          elevation: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+                height: 190,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 31,
+                        color: Colors.grey,
                       ),
-                    )
-                  ],
-                ),
-              )),
+                      Text(
+                        "Add a card",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AddCardScreen()));
+      },
     );
   }
 }
