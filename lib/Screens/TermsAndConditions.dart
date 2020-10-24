8c56201cc643e6f041c3687be499131e8ef46daf
dombroks <tinoutech@gmail.com>
2020-10-25 00:45:21 +0100

    ++

--- lib/Screens/TermsAndConditions.dart
+++ lib/Screens/TermsAndConditions.dart
@@ -17,13 +17,59 @@ class TermsConditions extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(18.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "Product Terms",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed rhoncus mi. Nullam enim arcu, feugiat eget arcu a, sagittis pretium purus. Integer ac eros vel odio aliquet imperdiet at nec leo. Sed ac diam nisi. Phasellus tristique, libero sed porta consequat, nulla est accumsan libero, eget ultrices lectus elit ut lorem. Mauris vitae nulla non quam efficitur tempor et id metus."),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "1. Use of the Site",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed rhoncus mi. Nullam enim arcu, feugiat eget arcu a, sagittis pretium purus. Integer ac eros vel odio aliquet imperdiet at nec leo. Sed ac diam nisi. Phasellus tristique, libero sed porta consequat, nulla est accumsan libero, eget ultrices lectus elit ut lorem. Mauris vitae nulla non quam efficitur tempor et id metus."),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "THESR TERMS AND CONDITIONS DO NOT AFFECT YOUR STATUTORY RIGHTS.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "1. Amendments",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                  "We may revise the terms and conditions from time to time without notice to you Lorem ipsum dolor sit")
             ],
           ),
         ),
