62bf0ab89aed23c39ffeaba8cc5ef4af3086a84f
dombroks <tinoutech@gmail.com>
2020-10-24 23:46:39 +0100

    ++

--- lib/Screens/ChangePasswordScreen.dart
+++ lib/Screens/ChangePasswordScreen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/constants.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kPrimaryColor,
         leading: Icon(
           Icons.close,
           color: Colors.grey,
