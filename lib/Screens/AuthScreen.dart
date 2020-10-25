98aa2a32cce589538e4729e22aca3269eaa4b72c
dombroks <tinoutech@gmail.com>
2020-10-25 18:25:30 +0100

    ++

--- lib/Screens/AuthScreen.dart
+++ lib/Screens/AuthScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/components/MyButton.dart';
+import 'package:grocery_app/components/Sign.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -40,62 +40,8 @@ class _AuthScreenState extends State<AuthScreen> {
         ),
         actions: [Text("Sign up")],
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(18),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Sign in to Grocery App",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "Enter email & password to continue",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: "Email address"),
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: "Password"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Forgot password?",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  MyButton("SIGN IN"),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+      body: Sign(),
     );
   }
 }
+
