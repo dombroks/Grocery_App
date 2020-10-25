66be05f0331002bec849b78adfb4fb5917f1e63d
dombroks <tinoutech@gmail.com>
2020-10-25 18:57:25 +0100

    ++

--- lib/components/SignIn.dart
+++ lib/components/SignIn.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'MyButton.dart';
 
 class SignIn extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Container(
