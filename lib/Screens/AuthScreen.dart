2256f2d0de9db58b83e4874f983788814ef7973c
dombroks <tinoutech@gmail.com>
2020-10-25 22:52:48 +0100

    ++

--- lib/Screens/AuthScreen.dart
+++ lib/Screens/AuthScreen.dart
@@ -7,44 +7,43 @@ import '../constants.dart';
 // ignore: must_be_immutable
 class AuthScreen extends StatelessWidget {
   String _inOrOut;
+
   AuthScreen(String _inOrOut) {
     this._inOrOut = _inOrOut;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0, left: 0),
-              child: Text(
-                "Sign in",
-                style: TextStyle(
-                    color: (_inOrOut == "In") ? kPrimaryColor : Colors.grey,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: TabBar(indicatorColor: kPrimaryColor, tabs: [
+              Tab(
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0, right: 0),
-              child: Text(
-                "Sign up",
-                style: TextStyle(
-                    color: (_inOrOut == "In") ? Colors.grey : kPrimaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-        actions: [Text("Sign up")],
-      ),
-      body: (_inOrOut == "In") ? SignIn() : SignUp(),
+              Tab(
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              )
+            ]),
+            actions: [Text("Sign up")],
+          ),
+          body: TabBarView(children: [
+            SignIn(),
+            SignUp(),
+          ])),
     );
   }
 }
