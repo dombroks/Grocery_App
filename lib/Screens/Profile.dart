cd7f874c48a53ffdfadc1415374f3cac99a59c00
dombroks <tinoutech@gmail.com>
2020-10-29 00:03:15 +0100

    ++

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
+import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_app/Provider/Mediator.dart';
 import 'package:grocery_app/Screens/AddPaymentMethod.dart';
 import 'package:grocery_app/Screens/AuthScreen.dart';
 import 'package:grocery_app/Screens/ChangePasswordScreen.dart';
 import 'package:grocery_app/Screens/RecipientDetails.dart';
-import 'package:grocery_app/Screens/SplashScreen.dart';
 import 'package:grocery_app/components/ProfileCardItem.dart';
 import 'package:grocery_app/constants.dart';
 import 'package:image_picker/image_picker.dart';
@@ -20,173 +20,177 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   final picker = ImagePicker();
   File _image;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      color: Colors.grey[200],
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10),
-            width: double.infinity,
-            height: size.height * 0.15,
-            color: Colors.white,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                GestureDetector(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: kPrimaryColor,
-                        )),
-                    width: 90,
-                    height: 90,
-                    child: _image == null
-                        ? Icon(
-                            Icons.camera,
+    return Scaffold(
+      body: Container(
+        color: Colors.grey[200],
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              height: size.height * 0.15,
+              color: Colors.white,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
                             color: kPrimaryColor,
-                            size: 40,
-                          )
-                        : ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.file(
-                              _image,
-                              fit: BoxFit.cover,
+                          )),
+                      width: 90,
+                      height: 90,
+                      child: _image == null
+                          ? Icon(
+                              Icons.camera,
+                              color: kPrimaryColor,
+                              size: 40,
+                            )
+                          : ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.file(
+                                _image,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
+                    ),
+                    onTap: () {
+                      getImage();
+                    },
                   ),
-                  onTap: () {
-                    getImage();
-                  },
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Center(
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Jessica alison",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 23,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "tinoutech@gmail.com",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "0777 000 000",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Jessica alison",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 23,
+                    children: [
+                      GestureDetector(
+                        child: ProfileCardItem(
+                          itemName: "Recipient details",
+                          colordata: Colors.purple,
+                          icon: Icons.account_circle,
                         ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RecipientDatails()));
+                        },
                       ),
                       SizedBox(
-                        height: 5,
+                        height: 0.5,
                       ),
-                      Text(
-                        "tinoutech@gmail.com",
-                        style: TextStyle(
-                          color: Colors.grey,
+                      GestureDetector(
+                        child: ProfileCardItem(
+                          itemName: "Delivery address",
+                          colordata: Colors.blue,
+                          icon: Icons.local_shipping,
                         ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RecipientDatails()));
+                        },
                       ),
                       SizedBox(
-                        height: 5,
+                        height: 0.5,
                       ),
-                      Text(
-                        "0777 000 000",
-                        style: TextStyle(
-                          color: Colors.grey,
+                      GestureDetector(
+                        child: ProfileCardItem(
+                          itemName: "Payment Method",
+                          colordata: Colors.green,
+                          icon: Icons.monetization_on,
                         ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddPaymentMethod()));
+                        },
+                      ),
+                      SizedBox(
+                        height: 0.5,
+                      ),
+                      GestureDetector(
+                        child: ProfileCardItem(
+                          itemName: "Change password",
+                          colordata: Colors.pink[300],
+                          icon: Icons.lock,
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ChangePasswordScreen()));
+                        },
+                      ),
+                      SizedBox(
+                        height: 0.5,
+                      ),
+                      GestureDetector(
+                        child: ProfileCardItem(
+                          itemName: "Logout",
+                          colordata: Colors.red[700],
+                          icon: Icons.exit_to_app,
+                        ),
+                        onTap: () {
+                          _showAlertDialg();
+                        },
                       ),
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      child: ProfileCardItem(
-                        itemName: "Recipient details",
-                        colordata: Colors.purple,
-                        icon: Icons.account_circle,
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RecipientDatails()));
-                      },
-                    ),
-                    SizedBox(
-                      height: 0.5,
-                    ),
-                    GestureDetector(
-                      child: ProfileCardItem(
-                        itemName: "Delivery address",
-                        colordata: Colors.blue,
-                        icon: Icons.local_shipping,
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RecipientDatails()));
-                      },
-                    ),
-                    SizedBox(
-                      height: 0.5,
-                    ),
-                    GestureDetector(
-                      child: ProfileCardItem(
-                        itemName: "Payment Method",
-                        colordata: Colors.green,
-                        icon: Icons.monetization_on,
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AddPaymentMethod()));
-                      },
-                    ),
-                    SizedBox(
-                      height: 0.5,
-                    ),
-                    GestureDetector(
-                      child: ProfileCardItem(
-                        itemName: "Change password",
-                        colordata: Colors.pink[300],
-                        icon: Icons.lock,
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ChangePasswordScreen()));
-                      },
-                    ),
-                    SizedBox(
-                      height: 0.5,
-                    ),
-                    GestureDetector(
-                      child: ProfileCardItem(
-                        itemName: "Logout",
-                        colordata: Colors.red[700],
-                        icon: Icons.exit_to_app,
-                      ),
-                      onTap: () {
-                        _showAlertDialg();
-                      },
-                    ),
-                  ],
-                ),
-              ))
-        ],
+                ))
+          ],
+        ),
       ),
     );
   }
