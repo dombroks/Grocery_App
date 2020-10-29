fd414a447df3fae9f557e42eb88a9b02bde38302
dombroks <tinoutech@gmail.com>
2020-10-29 18:06:01 +0100

    ++

--- lib/Provider/Mediator.dart
+++ lib/Provider/Mediator.dart
@@ -152,4 +152,15 @@ class Mediator extends ChangeNotifier {
   Future signOut() async {
     await _auth.signOut();
   }
+
+  Future signUpWithEmailAndPassword(
+      String username, String email, String password) async {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    FirebaseUser user = result.user;
+    await _db.collection("Users").document(user.uid).setData({
+      'username': username,
+      'email': email,
+    });
+  }
 }
