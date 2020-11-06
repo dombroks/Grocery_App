a237a738d63c174eb4a5fb4907636e257c7e6832
dombroks <tinoutech@gmail.com>
2020-11-06 23:54:41 +0100

    ++

--- lib/components/DrawerListTile.dart
+++ lib/components/DrawerListTile.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class DrawerListTile extends StatelessWidget {
+  final Function onClick;
+  final IconData iconData;
+  final String title;
   const DrawerListTile({
-    Key key,
+    Key key, this.onClick, this.iconData, this.title,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        "Order history",
+        title,
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
       ),
       leading: Container(
@@ -22,7 +25,7 @@ class DrawerListTile extends StatelessWidget {
           shape: BoxShape.circle,
         ),
         child: Icon(
-          Icons.history,
+          iconData,
           color: kPrimaryColor,
           size: 30,
         ),
