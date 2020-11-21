c4af82cd998f726c2539c2b92c6bb03995b12da8
dombroks <tinoutech@gmail.com>
2020-11-21 20:12:08 +0100

    ++

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/constants.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kPrimaryColor,
         leading: Icon(
           Icons.search,
           color: Colors.white,
         ),
-        title: TextField(),
+        title: TextField(
+          cursorColor: Colors.white,
+          style: TextStyle(color: Colors.white),
+          maxLines: 1,
+          decoration: InputDecoration.collapsed(hintText: "Search"),
+        ),
+        actions: [
+          Icon(Icons.mic),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(Icons.close)
+        ],
       ),
-      
     );
   }
 }
