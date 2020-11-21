2bb329739547c84f6b74bb13ab8e2f4d75c70a7a
dombroks <tinoutech@gmail.com>
2020-11-21 19:18:27 +0100

    ++

--- lib/Screens/AddPaymentMethod.dart
+++ lib/Screens/AddPaymentMethod.dart
@@ -1,11 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
-import 'package:grocery_app/Model/CreditCart.dart';
 import 'package:grocery_app/Provider/Mediator.dart';
-import 'package:grocery_app/components/AddCard.dart';
 import 'package:grocery_app/components/CreditCard.dart';
-
 import 'package:grocery_app/constants.dart';
 import 'package:provider/provider.dart';
 
